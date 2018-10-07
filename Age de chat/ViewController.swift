42ae29af128a652e255b390375e8631a48530d92
Nicolas Afonso <afonso.nicolas@live.fr>
2018-10-07 14:12:45 +0200

    Initial Commit

---
+++ Age de chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Age de chat
+//
+//  Created by Afonso, Nicolas on 01/10/2018.
+//  Copyright © 2018 CA Technologies Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
